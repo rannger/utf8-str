3ee1a07da5bf236caaa5babdf4460c021c650721
rannger <liang.rannger@gmail.com>
2017-07-11 12:57:36 +0800

    clean

--- utf8_str.c
+++ utf8_str.c
@@ -3,58 +3,6 @@
 #include <strings.h>
 #include <assert.h>
 
-typedef struct linklist_item {
-		utf8_char* ch;
-		struct linklist_item* next;
-} linklist_item;
-
-typedef struct linklist {
-		int count;
-		linklist_item* head;
-		linklist_item* tail;
-} linklist;
-
-static linklist* alloc_linklist(void)
-{
-	linklist* list = (linklist*)malloc(sizeof(linklist));
-	list->count = 0;
-	list->head = NULL;
-	list->tail = NULL;
-	return list;
-}
-
-static void linklist_append(linklist* list,linklist_item* item)
-{
-		if (0 == list->count) {
-				list->count = 1;
-				list->head = item;
-				list->tail = item;
-		} else {
-				list->count++;
-				list->tail->next = item;
-				list->tail = item;
-		}
-}
-
-static void linklist_release(linklist* list)
-{
-		while(list->count!=0) {
-			linklist_item* item = list->head;
-			list->head = list->head->next;
-			free(item);
-			list->count--;
-		}
-		free(list);
-}
-
-static linklist_item* alloc_linklist_item(utf8_char* ch)
-{
-		linklist_item* ret = (linklist_item*)malloc(sizeof(linklist_item));
-		ret->ch = ch;
-		ret->next = NULL;
-		return ret;
-}
-
 int char_len(unsigned char ch)
 {
 		int ret = 1;
@@ -81,25 +29,29 @@ void utf8_char_free(utf8_char* ch)
 utf8_char** rd_utf8_str(unsigned char* buf,int size)
 {
 	int index = 0;
-	linklist* list = alloc_linklist();
+	uint32_t item_size = 0;
 	do {
 		int len = char_len(buf[index]);
-		utf8_char* ch = alloc_char(len);
-		strncpy((char*)&(ch->data[0]),(const char*)buf+index,ch->len);
 		index += len;
-		linklist_append(list,alloc_linklist_item(ch));
+		++item_size;
 		if (index-1==size) 
 				break;
 	}while(1);
 
-	utf8_char** utf8_str = (utf8_char**)malloc(sizeof(utf8_char*) * (list->count+1));
-	linklist_item* item = list->head;
-	for (int i = 0;i<list->count;++i) {
-		utf8_str[i] = item->ch;
-		item = item->next;
-	}
-	utf8_str[list->count] = NULL;
-	linklist_release(list);
+	utf8_char** utf8_str = (utf8_char**)malloc(sizeof(utf8_char*) * (item_size+1));
+	index = 0;
+	int item_index = 0;
+	do {
+		int len = char_len(buf[index]);
+		utf8_char* ch = alloc_char(len);
+		strncpy((char*)&(ch->data[0]),(const char*)buf+index,ch->len);
+		utf8_str[item_index++] = ch;
+		index += len;
+		if (index-1==size) 
+				break;
+	} while(1);
+	
+	utf8_str[item_size] = NULL;
 
 	return utf8_str;
 }
